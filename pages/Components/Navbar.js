9d1cc5e1c76c31d3bc09342f84ad217926d8a556
unknown <melikdev@gmail.com>
2022-09-06 03:54:24 +0300

    changed logo

--- pages/Components/Navbar.js
+++ pages/Components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
       <div className={styles.logo}>
         <Link crossorigin="anonymous" href="/">
           <a>
-            <Image crossorigin="anonymous" src="/logo.png" height={60} width={220} alt="img" />
+            <Image crossOrigin src="/logo.png" height={60} width={220} alt="img" />
           </a>
         </Link>
       </div>
