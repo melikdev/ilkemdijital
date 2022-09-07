afa9837ba8aab106b3ce62ab0f1934c05a9476ec
unknown <melikdev@gmail.com>
2022-09-08 00:23:49 +0300

    added some features to the contact page form

--- pages/Components/Iletisim.js
+++ pages/Components/Iletisim.js
@@ -4,6 +4,7 @@ const Iletisim = () => {
   return (
     <div className={styles.container}>
       <div className={styles.form}>
+        <p>Sormuş olduğunuz ürünün fiyat bilgisi, sistemimizde kullandığınız e-mail üzerinden tarafınıza bildirilecektir.</p>
         <label>Adınız: </label>
         <input type="text" />
         <label>Soyadınız: </label>
